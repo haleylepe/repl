bbff2e454caaf113e9a4736058dbabbf24a723e5
Your Name <you@example.com>
2024-07-11 10:39:23 -0400

    added new RecoverableErr and IrrecoverableErr & respective def & goals

--- Semantics.v
+++ Semantics.v
@@ -1,13 +1,32 @@
 Require Import List.
+Require Import String. 
 Import ListNotations.
 From Repl Require Import
   (* Moqtop
   Result *) 
   Syntax.
 
+
 Inductive result : Type :=
 | success (gs : list goal)
-| failure.
+(* | failure - took out to have multiple types of errors *) 
+(* added two types of failer recoverable vs irrecoverable*)
+| recoverableErr (* "TODO" not sure if we need to add exception as a parameter here
+- did not add it currently because we do not need to throw an exception*)
+| irrecoverable (e: exception). 
+
+(* "Other TODO"  
+-look through stlc chapter about substitution, 
+- adding throw in result for irrecoverable type of plus
+- ignore exn type assume second tactic is always done (tbh 
+I do not remember why)
+*)
+(* also look possibly look at imp *)
+(* | unit (u : unit ) *)
+
+(* Definition thunk := unit -> a. *)
+(* Definition thunk := unit -> result. *)
+
 
 Parameter run_atomic : atomic -> goal -> result.
 
@@ -15,24 +34,36 @@ Inductive execute_atomic : atomic -> goal -> result -> Prop :=
 | e_atomic : forall a g,
   execute_atomic a g (run_atomic a g).
 
+  (* Definition execute_thunk (t : thunk) : result :=
+    t. *)
+
+
 Inductive execute_tactic : tactic -> goal -> result -> Prop :=
 | e_single : forall a g out,
   execute_atomic a g out ->
   execute_tactic (single a) g out
-| e_zero : forall g,
-  execute_tactic zero g failure
+| e_zero : forall g e ,
+  execute_tactic (zero e) g (recoverableErr) 
 | e_plus_okay1 : forall t1 t2 g outl1,
   execute_tactic t1 g (success outl1) ->
   execute_tactic (plus t1 t2) g (success outl1)
 | e_plus_okay2 : forall t1 t2 g outl2,
-  execute_tactic t1 g (failure) ->
+  execute_tactic t1 g (recoverableErr ) ->
   execute_tactic t2 g outl2 ->
-  (* ------------------------------- *)
-  execute_tactic (plus t1 t2) g (outl2).
+  execute_tactic (plus t1 t2) g (outl2)
+| e_throw : forall e g , 
+execute_tactic (throw e) g (irrecoverable e ) 
+(* "TODO" | create a new definition to take in here.
+Would this be  having a irrecoverableErr in the first opening of plus? *)
+| e_plus_notOkay1 : forall t1 t2 g e ,
+execute_tactic t1 g (irrecoverable e) ->
+execute_tactic (plus t1 t2) g (irrecoverable e). 
 
 
+(* Ensuring backtracking on plus, 
+with recoverable error/failure on first tactics *)
 Goal forall t1 t2 g outl2,
-  execute_tactic t1 g (failure) ->
+  execute_tactic t1 g (recoverableErr ) ->
   execute_tactic t2 g (success outl2) ->
   execute_tactic (plus t1 t2) g (success outl2).
 Proof.
@@ -42,11 +73,33 @@ apply e_plus_okay2.
 - apply H2.
 Qed.   
 
+(* Test if proof will only output first tactic that works *)
+Goal forall t1 t2 g outl1 outl2,
+execute_tactic t1 g (success outl1) ->
+execute_tactic t2 g (success outl2) ->
+execute_tactic (plus t1 t2) g (success outl1).
+Proof.
+intros. 
+apply e_plus_okay1. apply H.
+Qed.
 
+(* "NEW" ensure that the success t2 will not be
+ execute if t1 an irrecoverable error*)
+Goal forall t1 t2 g outl2 e,
+execute_tactic t1 g(irrecoverable e ) ->
+execute_tactic t2 g (success outl2) ->
+execute_tactic (plus t1 t2) g (irrecoverable e). 
+Proof. 
+intros.
+apply e_plus_notOkay1.
+- apply H.
+Qed.    
 
-(* | e_fail : forall g, 
-  execute_tactic "fail" g(failure [g])
-| e_first_nil : forall g, 
-   *)
+(* "TODO " trying to figure out how I can create a Goal for throw
+or if it is even conceptually possible*)
+Goal forall e g,
+execute_tactic . 
+Admitted. 
+
 
    
--- Syntax.v
+++ Syntax.v
@@ -7,11 +7,14 @@ Definition ident := string.
 Parameter atomic : Set.
 Parameter goal : Set.
 
+Inductive exception : Type :=
+| exn : string -> exception.
+
+Parameter unit : Set. 
+
 Inductive tactic : Type := 
 | single (a : atomic)
-(* | semicolon (t1 t2: tactic) *)
-(* | try (t1 : tactic) *)
-| zero (* todo: how to deal with thunking *)
-| plus (t1 t2 : tactic).
-(* | first (t1 : tactic) (t2 tactic_list). *)
-
+| zero (e : exception) (* Ltac2 Documentation: val zero : exn -> 'a*)
+| plus (t1 t2 : tactic) (*Ltac2 documentation: val plus
+ : (unit -> 'a) -> (exn -> 'a) -> 'a*)
+| throw (e : exception). (*ltac2 documentation: val throw : exn -> 'a*)
