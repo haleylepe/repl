b400cc48a171afa6449460328bca5198cd4ce871
Your Name <you@example.com>
2024-07-16 10:20:48 -0400

    commit before adding var exn to thunk

--- Syntax.v
+++ Syntax.v
@@ -7,14 +7,20 @@ Definition ident := string.
 Parameter atomic : Set.
 Parameter goal : Set.
 
-Inductive exception : Type :=
-| exn : string -> exception.
 
-Parameter unit : Set. 
+
+Inductive exception : Type :=
+(* | exn : string -> exception. *)
+| exn_f1
+| exn_f2
+| exn_var (x: string). 
+
+(* Parameter unit : Set.  *)
 
 Inductive tactic : Type := 
 | single (a : atomic)
 | zero (e : exception) (* Ltac2 Documentation: val zero : exn -> 'a*)
 | plus (t1 t2 : tactic) (*Ltac2 documentation: val plus
  : (unit -> 'a) -> (exn -> 'a) -> 'a*)
-| throw (e : exception). (*ltac2 documentation: val throw : exn -> 'a*)
+| throw (e : exception) (*ltac2 documentation: val throw : exn -> 'a*)
+| tMatch (e :exception) (handle1 handle2 : tactic). 
